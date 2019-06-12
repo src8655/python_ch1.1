395eb4f35f5dd2f777bbe7c493d400999129065d
src8655 <src8655@naver.com>
2019-06-12 13:11:15 +0900

    Initial commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+# hello.py
+
+
+def add(m, n):
+    s = m
+    s += n
+    return s
+
+
+def max(m, n):
+    if m > n:
+        return m
+    else:
+        return n
+
+
+a = 2
+b = 1
+
+if a > 1:
+    print('big')
+    print('big')
+
+    for i in range(1, 10):
+        print('-->', i)
+
+print('end')
+
